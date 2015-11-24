618793285c52195c4c6f7948fe5e21c5dab2268c
artefvck <artefvck07@gmail.com>
2015-11-24 16:41:41 +0700

    build cm clean up aosp

---
+++ cm.mk
@@ -0,0 +1,29 @@
+## Specify phone tech before including full_phone
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+# Release name
+PRODUCT_RELEASE_NAME := a400cg
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration change remove aosp full
+$(call inherit-product, device/asus/a400cg/aosp_a400cg.mk)
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+PRODUCT_NAME := cm_a400cg
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_T00I
+PRODUCT_MANUFACTURER := asus
+PRODUCT_DEVICE := a400cg
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+
+TARGET_VENDOR_PRODUCT_NAME := WW_ZenFone
+TARGET_VENDOR_DEVICE_NAME := ASUS_T00I
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=ASUS_T00I PRODUCT_NAME=WW_ZenFone
