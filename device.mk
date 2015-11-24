4a029f506cef1eb74a8991d8d118fe23f3a56da2
artefvck <artefvck07@gmail.com>
2015-11-24 15:51:42 +0700

    adjust cm overlay

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ endif
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel
 
+# cm overlay
+DEVICE_PACKAGE_OVERLAYS += device/asus/a400cg/overlay
+
 #bluetooth
 PRODUCT_COPY_FILES += \
         device/asus/a400cg/configs/bt_redhookbay_ttyMFD0.conf:system/etc/bluetooth/bt_redhookbay_ttyMFD0.conf \
